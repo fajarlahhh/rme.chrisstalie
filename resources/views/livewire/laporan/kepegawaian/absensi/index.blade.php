3da183191765a60a2e4c5d31b8628bee1f731d64
Andi Fajar Nugraha <andifajarlah@gmail.com>
2025-11-30 17:54:09 +0800

    Update date input fields in multiple laporan views to use wire:model.lazy for improved performance and responsiveness.

--- resources/views/livewire/laporan/kepegawaian/absensi/index.blade.php
+++ resources/views/livewire/laporan/kepegawaian/absensi/index.blade.php
@@ -27,9 +27,9 @@
                         <option value="Per Pegawai">Per Pegawai</option>
                     </select>
                     &nbsp;
-                    <input type="date" autocomplete="off" wire:model.debounce.500ms="tanggal1" id="tanggal"
+                    <input type="date" autocomplete="off" wire:model.lazy="tanggal1" id="tanggal"
                         class="form-control w-auto">&nbsp;s/d&nbsp;
-                    <input type="date" autocomplete="off" wire:model.debounce.500ms="tanggal2" id="tanggal"
+                    <input type="date" autocomplete="off" wire:model.lazy="tanggal2" id="tanggal"
                         class="form-control w-auto">&nbsp;
                     @if ($jenis == 'Per Pegawai')
                         <select class="form-control" wire:model.lazy="pegawai_id">
