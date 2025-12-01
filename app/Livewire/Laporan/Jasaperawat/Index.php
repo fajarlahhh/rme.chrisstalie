cb5a886fd9a3668d821f088494b712738435ed69
Andi Fajar Nugraha <andifajarlah@gmail.com>
2025-12-01 18:15:46 +0800

    Add filter to getData method in Jasaperawat component to exclude zero-cost services, improving data accuracy in exports.

--- app/Livewire/Laporan/Jasaperawat/Index.php
+++ app/Livewire/Laporan/Jasaperawat/Index.php
@@ -27,6 +27,7 @@ class Index extends Component
     public function getData()
     {
         return Tindakan::with('registrasi.pasien', 'pembayaran', 'perawat')
+            ->where('biaya_jasa_perawat', '>', 0)
             ->whereHas('pembayaran', fn($r) => $r
                 ->whereBetween(DB::raw('DATE(created_at)'), [$this->tanggal1, $this->tanggal2]))
             ->get()->map(fn($row) => [
